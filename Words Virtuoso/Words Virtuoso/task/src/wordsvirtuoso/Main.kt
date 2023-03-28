d5b67a211e58d18b0960af3134d65091497f7ff7
Evgenii Iarutin <evgenii.iarutin@area9.dk>
2023-03-28 13:29:37 +0500

    Words Virtuoso Step 4

--- Words Virtuoso/Words Virtuoso/task/src/wordsvirtuoso/Main.kt
+++ Words Virtuoso/Words Virtuoso/task/src/wordsvirtuoso/Main.kt
@@ -2,23 +2,73 @@ package wordsvirtuoso
 
 import java.io.File
 
-fun validateWord(word: String) = "[A-Za-z]{5}".toRegex().matchEntire(word) != null &&
-        word.groupBy { it }.all { it.value.size == 1 }
+fun hasWordInvalidSymbols(word: String) = "[A-Za-z]{5}".toRegex().matchEntire(word) == null
+fun hasWordDuplicateSymbols(word: String) = word.groupBy { it }.any { it.value.size != 1}
+fun isWordInvalid(word: String) = hasWordInvalidSymbols(word) || hasWordDuplicateSymbols(word)
 
-fun main() {
-    println("Input the words file:")
-    val filename = readln()
+fun error(message: String) {
+    println("Error: $message")
+    kotlin.system.exitProcess(1)
+}
 
-    val file = File(filename)
+fun validateFile(i: Int, file: File): File {
     if (!file.exists()) {
-        println("Error: The words file $filename doesn't exist.")
-        return
+        error("The ${if (i == 0) "" else "candidate "}words file ${file.name} doesn't exist.")
+    }
+    return file
+}
+fun readAndValidateWords(file: File): List<String> {
+    return file.readLines().map(String::lowercase) .also { words ->
+        words.count(::isWordInvalid).let {
+            if (it > 0) error("$it invalid words were found in the ${file.name} file.")
+        }
+    }
+}
+
+fun gameLoop(secret: String, words : Set<String>): Boolean {
+    println("\nInput a 5-letter word:")
+    val input = readln().lowercase()
+    if (input == secret) {
+        println("\nCorrect!")
+        return false
+    }
+    if (input == "exit") {
+        println("\nThe game is over.")
+        return false
+    } else if (input.length != 5) {
+        println("The input isn't a 5-letter word.")
+    } else if (hasWordInvalidSymbols(input)) {
+        println("One or more letters of the input aren't valid.")
+    } else if (hasWordDuplicateSymbols(input)) {
+        println("The input has duplicate letters.")
+    } else if (input !in words) {
+        println("The input word isn't included in my words list.")
+    } else {
+        println(input.mapIndexed { i, c ->
+            when (c) {
+                secret[i] -> c.uppercaseChar()
+                in secret -> c
+                else -> '_'
+            }
+        }.joinToString(""))
+    }
+    return true
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        error("Wrong number of arguments.")
+    }
+    val (words, candidates) = args.map { File(it) }.mapIndexed(::validateFile).map(::readAndValidateWords)
+        .map(List<String>::toSet)
+
+    candidates.count { it !in words }.let {
+        if (it > 0) error("$it candidate words are not included in the ${args[0]} file.")
     }
 
-    val (_, invalid) = file.readLines().partition(::validateWord)
-    if (invalid.isEmpty()) {
-        println("All words are valid!")
-    } else {
-        println("Warning: ${invalid.size} invalid words were found in the $filename file.")
-    }
+    println("Words Virtuoso")
+
+    val secret = candidates.random()
+
+    while (gameLoop(secret, words)) {}
 }
